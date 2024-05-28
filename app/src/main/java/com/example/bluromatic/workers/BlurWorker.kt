1d7cb275c5a91ab58c3fcea4c50b0b3325cafae5
csldeny <sldenycom@gmail.com>
2024-05-27 19:00:51 -0600

    Creacion del archivo BlurWorker con algo de codigo

---
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -0,0 +1,28 @@
+package com.example.bluromatic.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.bluromatic.R
+
+private const val TAG = "BlurWorker"
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    override suspend fun doWork(): Result {
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.blurring_image),
+            applicationContext
+        )
+        return try {
+            val picture = BitmapFactory.decodeResource(
+                applicationContext.resources,
+                R.drawable.android_cupcake
+            )
+
+            Result.success()
+        } catch (throwable: Throwable) {
+            Result.failure()
+        }
+    }
+}
