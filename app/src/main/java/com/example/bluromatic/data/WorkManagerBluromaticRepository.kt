cd170ae305747adf3c135cc0997293aefe35ac57
csldeny <sldenycom@gmail.com>
2024-05-27 22:21:09 -0600

    Datos de entrada y datos de salida

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -24,12 +24,14 @@ import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
 import com.example.bluromatic.workers.BlurWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
+    private var imageUri: Uri = context.getImageUri()
     private val workManager = WorkManager.getInstance(context)
 
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
@@ -42,7 +44,9 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
         // Create WorkRequest to blur the image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
 
-        // Start the work
+        // New code for input data object
+        blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+
         workManager.enqueue(blurBuilder.build())
     }
 
